45281222acfd1541e75833a79e9b6c940c420ad7
Your Name <youremail@yourdomain.com>
2023-10-01 02:51:11 +0600

    commit

--- bin/october.dart
+++ bin/october.dart
@@ -1,5 +1,92 @@
 import 'package:october/october.dart' as october;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${october.calculate()}!');
-}
+void main() {}
+// {
+ // print('Введите первое число');
+ // String start = stdin.readLineSync() ?? '';
+  //int summ = 0;
+  //for (int i = 0; i < start.length; i++) {
+    //summ += int.tryParse(start[i]) ?? 0;
+  //}
+  //print(summ);
+//}
+
+
+
+
+
+
+// {
+  //print('Введите первое число');
+  //int start = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+  //print('Введите второе число');
+  //int end = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+  //int summ = 0;
+  //for (int i = start; i <= end; i++) {
+    //summ += i;
+  //}
+  //print(summ);
+//}
+
+
+
+
+
+
+
+//{
+  //print(taskFour('a', 'daart'));
+//}
+//
+//int taskFour(String first, String second) {
+  //int resut = 0;
+  //for (int i = 0; i < second.length; i++) {
+    //if (second[i] == first) {
+     // resut++;
+    //}
+  //}
+  //return resut;
+//}
+
+
+
+
+
+
+
+
+
+// {
+  //List<int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+ // List result = [];
+//
+  //for (int i = 0; i < a.length; i++) {
+    //if (a[i].isEven) {
+      //result.add(a[i]);
+    //}
+  //}
+  //print(result);
+//}
+
+
+
+
+
+
+// {
+  //List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  //List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  //List result = [];
+//
+  //for (int i = 0; i < a.length; i++) {
+    //if (b.contains(
+      //a[i],
+    //)) {
+      //result.add(
+        //a[i],
+      //);
+    //}
+  //}
+  //print(result.toSet().toList());
+//}
